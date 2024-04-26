027fa31bc6d290b3653b59c4fef190404919f1ef
brenokm <brenomatostrabalho@gmail.com>
2024-04-26 19:49:28 -0300

    first commit

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SHOPIX</title>
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> --}}
+    <link rel="stylesheet" href="{{ URL::asset('css/headerStyles.css') }}">
+    
+    
+
+</head>
+<body>
+    <header class="bg-warning">
+        
+        <h1 id="logotipo">SHOPIX</h1>
+        <ol>
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Shop</a></li>
+            <li><a href="">Masculino</a></li>
+            <li><a href="">Feminino</a></li>
+            <input class="" type="text" placeholder="Pesquisar">
+        </ol>
+        <ol>
+            <li><a href="">Em Promoção</a></li>
+            <li><a href="">Empresas</a></li>
+            <li><a href="">Frete grátis</a></li>
+            <li><a href="">Desconto à vista</a></li>
+        </ol>
+    </header>
+
+</body>
+</html>
